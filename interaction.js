9a40e0373bde71f2b104942af33427d3a60908f7
kitemich <michalmaciejewski@MBP-Micha.stamkartplein.duwo.lan>
2016-12-16 23:21:12 +0100

    Scroll Reveal
    I was playing with scroll reveal and added it to my website so it’s
    more interactive now

---
+++ interaction.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal();
+sr.reveal('.row1');
+sr.reveal('.row2');
+sr.reveal('.row3');
+sr.reveal('.row4');
+sr.reveal('.row5');
+
+sr.reveal('#video');
+
+sr.reveal('.contactFirst');
+
+sr.reveal('#map');
+
+
+
+
